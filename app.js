6c4b2a02b5d83a825c666a5fca826d68662971dd
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-12-07 13:16:34 -0500

    serves reserve.html to browser from server, created blank objext for form input

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const path = require("path");
 
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`App is listening on port + ${PORT}`);
-} );
+});
 
-const newResverations = [];
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "front", "reserve.html"));
+});
+
 
 
